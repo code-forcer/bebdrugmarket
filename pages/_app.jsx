5b2d1939ff4ea772ba949449d7b1b5f3a20a77f2
code-forcer <135433507+code-forcer@users.noreply.github.com>
2024-06-29 16:10:58 +0100

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -9,9 +9,6 @@ function MyApp({ Component, pageProps }) {
         <meta content="BeBDrugStore for pharmacy and doctor relationship" name="description"/>
    </Head>
    <header className="header">
-        <div id="spinner" class="show w-100 vh-100 bg-white position-fixed translate-middle top-50 start-50  d-flex align-items-center justify-content-center">
-            <div class="spinner-grow text-primary" role="status"></div>
-        </div>
         <div class="container-fluid fixed-top">
             <div class="container topbar bg-primary d-none d-lg-block">
                 <div class="d-flex justify-content-between">
